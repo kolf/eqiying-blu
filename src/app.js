35cad3c6c918526c3c91b856908414cbfe2d0b6b
石先康 <xiankang.shi@visualchina.com>
2017-04-13 20:50:02 +0800

    build

--- src/app.js
+++ src/app.js
@@ -1,1 +1,38 @@
-app.js
+import Vue from 'vue'
+import NProgress from 'vue-nprogress'
+import { sync } from 'vuex-router-sync'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import { TOGGLE_PAGE, TOGGLE_SIDEBAR } from 'vuex-store/mutation-types'
+import Promise from 'es6-promise'
+
+Promise.polyfill()
+Vue.use(NProgress)
+
+// Enable devtools
+Vue.config.devtools = true
+
+sync(store, router)
+
+const nprogress = new NProgress({ parent: '.nprogress-container' })
+
+const { state } = store
+
+router.beforeEach((route, redirect, next) => {
+  store.commit(TOGGLE_PAGE, route.path.split('/')[1])
+  if (state.app.device.isMobile && state.app.sidebar.opened) {
+    store.commit(TOGGLE_SIDEBAR, false)
+  }
+  next()
+})
+
+const app = new Vue({
+  i18n,
+  router,
+  store,
+  nprogress,
+  ...App
+})
+
+export { app, router, store }
--- src/main.js
+++ src/main.js
@@ -1,27 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import VueBlu from 'vue-blu'
-import NProgress from 'vue-nprogress'
-import zh from 'vee-validate/dist/locale/zh_CN'
-import VeeValidate, { Validator } from 'vee-validate'
-import VueLazyload from 'vue-lazyload'
-import { sync } from 'vuex-router-sync'
-import App from './App'
-import router from './router'
+import { app } from './app'
 
-Vue.config.productionTip = false
-
-Validator.addLocale(zh)
-Vue.use(VueBlu)
-Vue.use(VueLazyload)
-Vue.use(VeeValidate, {
-  locale: 'zh_CN'
-})
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
-})
+app.$mount('#app')
